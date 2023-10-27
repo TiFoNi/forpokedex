4ae67150f645e6d755165ed0697c3e23ab522e71
TiFoNi <iitbgtifoni@gmail.com>
2023-10-27 13:04:55 +0300

    Update page.tsx

--- src/app/FavoritesPokemons/page.tsx
+++ src/app/FavoritesPokemons/page.tsx
@@ -5,21 +5,12 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import s from "./page.module.scss";
 
 import { useEffect } from "react";
-import { NavbarProvider, useNavbar } from "@/components/Navbar/NavbarContext";
 
 function favPage() {
-  // const { setActivePage } = useNavbar(); // Отримайте setActivePage з контексту
-
-  // useEffect(() => {
-  //   setActivePage(2);
-  // }, []);
-
   return (
-    // <NavbarProvider>
     <div className={s.Global}>
       <Test />
     </div>
-    // </NavbarProvider>
   );
 }
 
